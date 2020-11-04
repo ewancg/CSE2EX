eaca1b0ed38c8b5f62244879c2743dc255895dc4
Clownacy <Clownacy@users.noreply.github.com>
2020-11-04 19:00:18 +0000

    Simplify 3DS controller logic

--- src/Backends/Controller/3DS.cpp
+++ src/Backends/Controller/3DS.cpp
@@ -27,36 +27,34 @@ bool ControllerBackend_GetJoystickStatus(bool **buttons, unsigned int *button_co
 	*button_count = sizeof(button_buffer) / sizeof(button_buffer[0]);
 	*axis_count = 0;
 
-	memset(button_buffer, 0, sizeof(button_buffer));
-
 	// Read controller
 	hidScanInput();
 
 	u32 hid_buttons = hidKeysHeld();
 
 	// Handle button inputs
-	button_buffer[0] |= hid_buttons & KEY_Y;          // Shoot
-	button_buffer[1] |= hid_buttons & KEY_B;          // Jump
-	button_buffer[2] |= hid_buttons & KEY_X;          // Map
-	button_buffer[3] |= hid_buttons & KEY_L;          // Weapon left
-	button_buffer[4] |= hid_buttons & KEY_R;          // Weapon right
-	button_buffer[5] |= hid_buttons & KEY_A;          // Inventory
-	button_buffer[6] |= hid_buttons & KEY_START;      // Pause
-	button_buffer[7] |= hid_buttons & KEY_CPAD_UP;    // Up
-	button_buffer[8] |= hid_buttons & KEY_CPAD_DOWN;  // Down
-	button_buffer[9] |= hid_buttons & KEY_CPAD_LEFT;  // Left
-	button_buffer[10] |= hid_buttons & KEY_CPAD_RIGHT;// Right
-	button_buffer[11] |= hid_buttons & KEY_ZL;
-	button_buffer[12] |= hid_buttons & KEY_ZR;
-	button_buffer[13] |= hid_buttons & KEY_SELECT;
-	button_buffer[14] |= hid_buttons & KEY_DLEFT;
-	button_buffer[15] |= hid_buttons & KEY_DRIGHT;
-	button_buffer[16] |= hid_buttons & KEY_DUP;
-	button_buffer[17] |= hid_buttons & KEY_DDOWN;
-	button_buffer[18] |= hid_buttons & KEY_CSTICK_LEFT;
-	button_buffer[19] |= hid_buttons & KEY_CSTICK_RIGHT;
-	button_buffer[20] |= hid_buttons & KEY_CSTICK_UP;
-	button_buffer[21] |= hid_buttons & KEY_CSTICK_DOWN;
+	button_buffer[0] = hid_buttons & KEY_Y;          // Shoot
+	button_buffer[1] = hid_buttons & KEY_B;          // Jump
+	button_buffer[2] = hid_buttons & KEY_X;          // Map
+	button_buffer[3] = hid_buttons & KEY_L;          // Weapon left
+	button_buffer[4] = hid_buttons & KEY_R;          // Weapon right
+	button_buffer[5] = hid_buttons & KEY_A;          // Inventory
+	button_buffer[6] = hid_buttons & KEY_START;      // Pause
+	button_buffer[7] = hid_buttons & KEY_CPAD_UP;    // Up
+	button_buffer[8] = hid_buttons & KEY_CPAD_DOWN;  // Down
+	button_buffer[9] = hid_buttons & KEY_CPAD_LEFT;  // Left
+	button_buffer[10] = hid_buttons & KEY_CPAD_RIGHT;// Right
+	button_buffer[11] = hid_buttons & KEY_ZL;
+	button_buffer[12] = hid_buttons & KEY_ZR;
+	button_buffer[13] = hid_buttons & KEY_SELECT;
+	button_buffer[14] = hid_buttons & KEY_DLEFT;
+	button_buffer[15] = hid_buttons & KEY_DRIGHT;
+	button_buffer[16] = hid_buttons & KEY_DUP;
+	button_buffer[17] = hid_buttons & KEY_DDOWN;
+	button_buffer[18] = hid_buttons & KEY_CSTICK_LEFT;
+	button_buffer[19] = hid_buttons & KEY_CSTICK_RIGHT;
+	button_buffer[20] = hid_buttons & KEY_CSTICK_UP;
+	button_buffer[21] = hid_buttons & KEY_CSTICK_DOWN;
 
 	return true;
 }
